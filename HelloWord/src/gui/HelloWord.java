386bbddfb90a791c1f5c95da8cc63fc96ad55e89
Al3xmm14 <molinaa757@gmail.com>
2022-08-23 19:32:45 -0500

    Hola Mundo

---
+++ HelloWord/src/gui/HelloWord.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gui;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author al3xmm14
+ * @version 23/08/2022
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null , "Hello Word");
+    }
+}
---
+++ HelloWord/src/txt/HellowordTxt.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package txt;
+
+/**
+ * @author al3xmm14
+ * @version 23/08/2022
+ *
+ */
+public class HellowordTxt {
+    public static void main(String[] args) {
+        System.out.println("Hello word!!");
+    }
+}
